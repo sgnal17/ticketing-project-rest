ec0e36c4aaa89a784022453f702d23462820c5ab
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-06-05 21:14:50 -0400

    ModelMapper bean added

--- src/main/java/com/cydeo/MyTicketingProjectRestApplication.java
+++ src/main/java/com/cydeo/MyTicketingProjectRestApplication.java
@@ -1,7 +1,9 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyTicketingProjectRestApplication {
@@ -10,4 +12,8 @@ public class MyTicketingProjectRestApplication {
         SpringApplication.run(MyTicketingProjectRestApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
